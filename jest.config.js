640fbd6762a256ffe1958b910316f926ea4cd897
William <wilz.kelvin12@gmail.com>
2022-10-06 11:32:13 -0300

    chore: :tada: add the necessary dependencies and start the project

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  testEnvironment: "jest-environment-node",
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+};
