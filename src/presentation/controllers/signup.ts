016e1e0d24b74b29736f4e7ae1dd6ff58e206a0f
William <wilz.kelvin12@gmail.com>
2022-10-06 17:37:41 -0300

    feat: :sparkles: add Account model and AddAccount usecase in SignUp controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,14 @@
 import { HttpRequest, HttpResponse, Controller, EmailValidator } from '../protocols';
 import { badRequest, serverError } from '../helpers/http-helper';
 import { InvalidParamError, MissingParamError } from '../errors';
+import { AddAccount } from '../../domain/usecases/add-account';
 export class SignUpController implements Controller {
-  private readonly emailValidator: EmailValidator
+  private readonly emailValidator: EmailValidator;
+  private readonly addAccount: AddAccount;
   
-  constructor(emailValidator: EmailValidator) {
-    this.emailValidator = emailValidator
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
+    this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -17,7 +20,7 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email, password, passwordConfirmation } = httpRequest.body;
+      const { name, email, password, passwordConfirmation } = httpRequest.body;
 
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
@@ -28,6 +31,11 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      this.addAccount.add({
+        name,
+        email,
+        password
+      })
     } catch (error) {
       return serverError()
     }
