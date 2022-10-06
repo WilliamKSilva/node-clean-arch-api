ef8d8af20f807bcecffad1fdeb7753d9b1022ffa
William <wilz.kelvin12@gmail.com>
2022-10-06 12:05:53 -0300

    test: :white_check_mark: return error in SignUp controller if is missing email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from "./signup"
 
 describe('Signup Controller', () => {
-  test('Should return 400 if no name is provided', () => {
+  it('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {        
@@ -13,5 +13,19 @@ describe('Signup Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  it('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {        
+        name: 'test',
+        password: 'test12345',
+        passwordConfirmation: 'test12345'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
