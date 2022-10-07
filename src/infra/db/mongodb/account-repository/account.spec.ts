f6a3af896e813faa5beac7fb3c4bca3d7b4ac93c
William <wilz.kelvin12@gmail.com>
2022-10-07 13:10:13 -0300

    test: :white_check_mark: ensure AccountMongoRepository returns an Account on success

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,25 @@
+import { MongoHelper } from '../helpers/mongo-helper';
+
+describe('Account Mongo Repository', () => {  
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  })
+
+  it('Should return an Account on success', async () => {
+    const sut = new AccountMongoRepository();
+    const account = await sut.add({
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test12345'
+    })
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('test');
+    expect(account.email).toBe('test@test.com');
+    expect(account.password).toBe('test12345');
+  })  
+})
