dfbca0e3042ff28ec7ff471bdd25f8dff5e3bd6f
William <wilz.kelvin12@gmail.com>
2022-10-06 22:22:44 -0300

    test: :white_check_mark: ensure DbAddAccount throws an error if Encrypter throws an error

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -37,4 +37,16 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(accountData);
     expect(encryptSpy).toHaveBeenCalledWith('test12345');
   })
+
+  it('Should throw an error if Encrypter throws an error', async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))    
+    const accountData = {
+      name: 'test',
+      email: 'test@test.com',
+      password: 'test12345'
+    };
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  })
 })
