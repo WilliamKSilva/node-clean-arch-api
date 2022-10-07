1767380c943f230a31e9122c965575fb07d710ef
William <wilz.kelvin12@gmail.com>
2022-10-07 11:41:33 -0300

    refactor: :recycle: change test name

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -83,7 +83,7 @@ describe('DbAddAccount Usecase', () => {
       password: 'hashed_password'
     })
   })
-  it('Should throw an error if', async () => {
+  it('Should throw an error if AddAccountRepository throws an error', async () => {
     const { sut, addAccountRepositoryStub } = makeSut();
     jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())));    
     const accountData = {
