86ffa76fa4222507e59e2890de0f416530ece74e
William <wilz.kelvin12@gmail.com>
2022-10-07 14:11:16 -0300

    feat: :sparkles: ensure AccountMongoRepository returns an Account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,22 @@
+import { AddAccountRepository } from "../../../../data/protocols/add-account-repository";
+import { AccountModel } from "../../../../domain/models/account";
+import { AddAccountModel } from "../../../../domain/usecases/add-account";
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    const account: AccountModel = {
+      id: '',
+      email: accountData.email,
+      name: accountData.name,
+      password: accountData.password
+    };    
+    const result = await accountCollection.insertOne(accountData);
+    account.id = result.insertedId.toJSON();
+
+    console.log(account);
+     
+    return account;
+  }
+}
