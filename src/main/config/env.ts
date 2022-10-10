3492122b1617642d7fbef7bae1a36ca088f97c5f
William <wilz.kelvin12@gmail.com>
2022-10-10 10:49:28 -0300

    chore: :wrench: setup docker with mongodb and api

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/node-clean-arch',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo_db:27017/node-clean-arch',
   port: process.env.PORT || 5050
 }
