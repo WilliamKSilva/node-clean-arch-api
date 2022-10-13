c73fa02c7df6d46c53edfac6825a410d3c7a3b30
William <wilz.kelvin12@gmail.com>
2022-10-13 15:13:53 -0300

    test: :white_check_mark: ensure log LogControllerDecorator calls controller with right data

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,35 @@
+import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols";
+import { LogControllerDecorator } from "./log"
+
+describe('LogController Decorator', () => {
+  it('Should call controller handle', async () => {
+    class ControllerStub implements Controller {
+      async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse: HttpResponse = {
+          statusCode: 200,
+          body: {
+            email: 'test@test.com',
+            name: 'test',
+            password: 'test12345',
+            passwordConfirmation: 'test12345'
+          }
+        }
+        return new Promise(resolve => resolve(httpResponse));
+      }
+    }
+
+    const controllerStub = new ControllerStub();
+    const handleSpy = jest.spyOn(controllerStub, 'handle'); 
+    const sut = new LogControllerDecorator(controllerStub);
+    const httpRequest = {
+      body: {
+        email: 'test@test.com',
+        name: 'test',
+        password: 'test12345',
+        passwordConfirmation: 'test12345'
+      }
+    }
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  })
+})
