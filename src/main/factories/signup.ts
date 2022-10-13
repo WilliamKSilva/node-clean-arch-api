0b0fcfc34fe089b0f111db4f7f763bfff4af1f89
William <wilz.kelvin12@gmail.com>
2022-10-13 15:13:44 -0300

    feat: :sparkles: ensure log LogControllerDecorator calls controller with right data

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,9 +2,12 @@ import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/cryptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account";
 import { SignUpController } from "../../presentation/controllers/signup/signup";
+import { Controller } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
+import { LogControllerDecorator } from "../decorators/log";
 
-export const makeSignUpController = (): SignUpController => {
+
+export const makeSignUpController = (): Controller => {
   const salt = 12;
   
   const emailValidatorAdapter = new EmailValidatorAdapter();
@@ -13,5 +16,5 @@ export const makeSignUpController = (): SignUpController => {
   const dbAddAccount = new DbAddAccount(encrypter, addAccountRepository);
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount);
 
-  return signUpController;
+  return new LogControllerDecorator(signUpController);
 }
