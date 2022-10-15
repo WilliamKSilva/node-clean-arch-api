ccb3501e8ac96231c28142043eb9283cf3e7026e
William <wilz.kelvin12@gmail.com>
2022-10-15 16:17:58 -0300

    test: :white_check_mark: ensure LoginController calls Authentication with right values

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,12 +1,26 @@
+import { Authentication, AuthenticationData } from "../../../domain/usecases/authentication";
 import { MissingParamError } from "../../errors";
 import { badRequest } from "../../helpers/http-helper";
+import { AccountModel } from "../signup/signup-protocols";
 import { LoginController } from "./login"
 
+const makeAuthentication = () => {
+  class AuthenticationStub implements Authentication {
+    async auth(authenticationData: AuthenticationData): Promise<string> {
+      return new Promise(resolve => resolve('test_token'));
+    }
+  }
+
+  return new AuthenticationStub();
+}
+
 const makeSut = () => {
-  const loginControllerStub = new LoginController();
+  const authenticationStub = makeAuthentication();
+  const loginControllerStub = new LoginController(authenticationStub);
 
   return {
-    loginControllerStub
+    loginControllerStub,
+    authenticationStub
   }
 }
 
@@ -37,16 +51,18 @@ describe('Login Controller', () => {
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
   })
 
-  it('Should return 400 if no password is provided', async () => {
-    const { loginControllerStub } = makeSut();
+  it('Should call authentication with right values', async () => {
+    const { loginControllerStub, authenticationStub } = makeSut();
     const httpRequest = {
       body: {
-        email: 'test@test.com',                
+        email: 'test@test.com',
+        password: 'test12345'                
       }
     }
 
-    const httpResponse = await loginControllerStub.handle(httpRequest);
+    const authenticationStubSpy = jest.spyOn(authenticationStub, 'auth');
+    await loginControllerStub.handle(httpRequest);
 
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
+    expect(authenticationStubSpy).toHaveBeenCalledWith(httpRequest.body);
   })
 })
