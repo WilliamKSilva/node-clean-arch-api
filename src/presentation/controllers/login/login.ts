72997a03e9545b71243a7cebcad3f0e46e7f3b30
William <wilz.kelvin12@gmail.com>
2022-10-15 15:53:41 -0300

    feat: :sparkles: ensure missing param error is return if email or password is not provided

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,24 @@
+import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers/http-helper";
+import { Controller, HttpRequest, HttpResponse } from "../../protocols";
+
+
+export class LoginController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError('email'));
+    }
+
+    if (!httpRequest.body.password) {
+      return badRequest(new MissingParamError('password'));
+    }
+    
+    return {
+      statusCode: 200,
+      body: {
+        email: 'test@test.com',
+        password: 'test12345'
+      }
+    }
+  }
+}
